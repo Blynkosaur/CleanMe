cb72775d36c54985d79534f02c60acb266a1d113
mini-bryanlin <bryanlin316@gmail.com>
2026-02-08 23:29:42 -0500

    done

--- Core/Source/Parser/Parser.cpp
+++ Core/Source/Parser/Parser.cpp
@@ -1,8 +1,9 @@
 #include "Parser.h"
 #include <filesystem>
-
+#include <system_error>
 class Parser {
 private:
+  std::error_code errorStatus;
   std::filesystem::path workingDirectory;
   std::filesystem::recursive_directory_iterator iterator;
   int folderSize{0};
@@ -11,7 +12,16 @@ private:
 public:
   Parser(const std::string inputPath)
       : workingDirectory{std::filesystem::path(inputPath)},
+        iterator{std::filesystem::recursive_directory_iterator(workingDirectory,
+                                                               errorStatus)} {
+    if (errorStatus) {
+      workingDirectory = std::filesystem::current_path();
+      iterator = std::filesystem::recursive_directory_iterator(
+          std::filesystem::current_path());
+    }
+  }
+  Parser()
+      : workingDirectory{std::filesystem::current_path()}, errorStatus{},
         iterator{
             std::filesystem::recursive_directory_iterator(workingDirectory)} {}
-  Parser() : workingDirectory{std::filesystem::current_path()} {}
 };
