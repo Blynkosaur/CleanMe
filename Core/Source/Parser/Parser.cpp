900d330d72ceebfe61daf14611a9d5b2d489d806
mini-bryanlin <bryanlin316@gmail.com>
2026-02-08 22:39:18 -0500

    feat: parser class

--- Core/Source/Parser/Parser.cpp
+++ Core/Source/Parser/Parser.cpp
@@ -1,3 +1,14 @@
 #include "Parser.h"
+#include <filesystem>
 
-class Parser {};
+class Parser {
+public:
+  Parser(const std::string inputPath)
+      : workingDirectory{std::filesystem::path(inputPath)} {}
+
+private:
+  std::filesystem::path workingDirectory;
+
+  int folderSize{0};
+  int errofFiles{0};
+};
