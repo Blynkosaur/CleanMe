d6d315bfcaa9e1b18e521e5652f7ebddc137618e
mini-bryanlin <bryanlin316@gmail.com>
2026-02-08 23:02:04 -0500

    constructor shinanigans

--- Core/Source/Parser/Parser.cpp
+++ Core/Source/Parser/Parser.cpp
@@ -2,13 +2,16 @@
 #include <filesystem>
 
 class Parser {
-public:
-  Parser(const std::string inputPath)
-      : workingDirectory{std::filesystem::path(inputPath)} {}
-
 private:
   std::filesystem::path workingDirectory;
-
+  std::filesystem::recursive_directory_iterator iterator;
   int folderSize{0};
   int errofFiles{0};
+
+public:
+  Parser(const std::string inputPath)
+      : workingDirectory{std::filesystem::path(inputPath)},
+        iterator{
+            std::filesystem::recursive_directory_iterator(workingDirectory)} {}
+  Parser() : workingDirectory{std::filesystem::current_path()} {}
 };
